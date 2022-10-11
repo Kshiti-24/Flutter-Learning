50b9dc1e92db63e365c1a0ba29e81dcafb84249d
Kshitiz <kshitizagarwa2405@gmail.com>
2022-10-12 03:54:13 +0530

    added catalog items

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,26 +1,36 @@
+import 'package:first/models/catalog.dart';
 import 'package:first/widgets/drawer.dart';
 import 'package:flutter/material.dart';
+import '../widgets/items_widget.dart';
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
  final int num=1;
  final String name="Kshitiz Agarwal";
   @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
+  Widget build (BuildContext context){
+  final dummyList=List.generate(
+      20, (index) => CatalogModel.items[0]);
+    return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-      title:Text("Catalog App",
-        style: Theme.of(context).textTheme.headline6,
-
+        title: Text("Catalog App",
+          style: Theme
+              .of(context)
+              .textTheme
+              .headline6,
+  ),
       ),
-      ),
-      body: Center(
-        child: Container(
-          child: Text("$num. Welcome to my first App by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child:  ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
-      drawer: MyDrawer(
-      ),
+      drawer: MyDrawer(),
     );
   }
 }
